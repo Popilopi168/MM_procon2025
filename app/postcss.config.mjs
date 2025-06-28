fe467c9a470258337922ac75241c1db72c492140
Popilopi168 <123021480+Popilopi168@users.noreply.github.com>
2025-06-28 00:50:50 -0400

    Tailwind CSS added

--- app/postcss.config.mjs
+++ app/postcss.config.mjs
@@ -1,11 +1,4 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // make sure jsx is included!
-  ],
-  theme: {
-    extend: {},
-  },
   plugins: {
     "@tailwindcss/postcss": {},
   }
