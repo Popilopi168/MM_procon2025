708a94f51ac81e43a1d45ea3970fcd97a8007ff7
Popilopi168 <123021480+Popilopi168@users.noreply.github.com>
2025-07-01 21:53:07 -0400

    minor UI fix

--- app/src/components/Chat/SuperChat.jsx
+++ app/src/components/Chat/SuperChat.jsx
@@ -3,6 +3,7 @@ import lyricIcon from "../../assets/lyric_icon.png";
 
 export default function SuperChat({ superMsg }) {
   const [visible, setVisible] = useState(false);
+  const donation = Math.floor(Math.random() * 100);
 
   useEffect(() => {
     if (!superMsg) return;
@@ -14,9 +15,13 @@ export default function SuperChat({ superMsg }) {
   if (!visible) return null;
 
   return (
-    <div className="absolute top-10 left-1/2 -translate-x-1/2 bg-yellow-400 text-black px-6 py-3 rounded-lg shadow-2xl animate-pulse flex flex-row gap-2">
-        <img src={lyricIcon} alt="avatar" className="w-5 h-5 rounded-full" />
-      {superMsg}
+    <div className="flex flex-col gap-2 absolute top-10 left-1/2 -translate-x-1/2 bg-yellow-400 text-black px-5 py-3 rounded-lg shadow-2xl animate-pulse">
+        <div className="flex flex-row gap-3">
+        <img src={lyricIcon} alt="avatar" className="w-5 h-5 rounded-full" /> <p className="text-sm">Miku fans  ${donation}</p>
+        </div>
+        <div className="bg-[#FEFFEF] rounded-lg p-2">
+            {superMsg}
+        </div>
     </div>
   );
 }
