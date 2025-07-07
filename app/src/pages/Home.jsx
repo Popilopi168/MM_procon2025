a89b61618a7a93bfcc4a4128c384863faeda38e0
Popilopi168 <123021480+Popilopi168@users.noreply.github.com>
2025-07-07 13:37:01 -0400

    home page decor added

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -2,14 +2,15 @@ import { useNavigate } from "react-router-dom";
 import redirectTutorial from "../assets/redirect_tutorial.png";
 import redirectCamera from "../assets/redirect_camera.png";
 import cover from "../assets/cover.png";
-import frame from "../assets/frame.png";
+import star from "../assets/star.png";
 
 function Home() {
     const navigate = useNavigate();
 
   return (
     <div className="flex flex-col bg-[#001d70] items-center justify-center h-screen gap-12 bg-gradient-to-b from-[#001d70] to-black">
-        <img src={frame} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] h-[88%] mt-6"></img>
+        <img src={star} className="absolute top-1/2 left-5 -translate-y-1/2 w-[15%]"/>
+        <img src={star} className="absolute top-1/2 right-5 -translate-y-1/2 w-[15%]"/>
         <button className="absolute top-5 left-5 hover:opacity-70 transition-opacity duration-300">
             <img src={redirectTutorial} onClick={() => navigate("/onboarding"+ window.location.search)}/>
         </button>
